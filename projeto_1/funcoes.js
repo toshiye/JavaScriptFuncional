55e42e5b5f93515e3a24707e4956cf808041989b
toshiye <sasakikenji@gmail.com>
2020-07-29 11:55:02 -0300

    sasaki_20200729

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -15,11 +15,63 @@ function lerDiretorio(caminho){
     })
 }
 
-function elementosTerminadosCom(array, padrao){
-    return array.filter(el => el.endsWith(padrao))
+function lerArquivo(caminho){
+    return new Promise((resolve, reject) => {
+        try{
+            const conteudo = fs.readFileSync(caminho, {encoding: 'utf-8'})
+            resolve(conteudo.toString())
+        } catch(e){
+            reject(e)
+        }
+    })
+}
+
+function lerArquivos(caminhos){
+    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+}
+
+function elementosTerminadosCom(padraoTextual){
+    return function(array){
+        return array.filter(el => el.endsWith(padraoTextual))
+    }
+}
+
+function removerElementosSeVazio(array){
+    return array.filter(el => el.trim())
+}
+
+function removerElementosSeIncluir(padraoTextual){
+    return function(array){
+        return array.filter(el => !el.includes(padraoTextual))
+    }
+}
+
+function removerElementosSeApenasNumero(array){
+    return array.filter(el => {
+        const num = parseInt(el.trim())
+        return num !== num
+    })
+}
+
+function removerSimbolos(simbolos){
+    return function(array){
+        return array.map(el => {
+            let textoSemSimbolos = el
+            simbolos.forEach(simbolo => {
+                textoSemSimbolos = textoSemSimbolos.split(simbolo).join('')
+            })
+            return textoSemSimbolos
+        })
+    }
 }
 
 module.exports = {
     lerDiretorio,
-    elementosTerminadosCom
+    lerArquivo,
+    lerArquivos,
+    elementosTerminadosCom,
+    removerElementosSeVazio,
+    removerElementosSeIncluir,
+    removerElementosSeApenasNumero,
+    removerSimbolos
 }
--- projeto_1/index.js
+++ projeto_1/index.js
@@ -3,6 +3,17 @@ const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
+const simbolos = [
+    '.', '?', '-', ',', '"', '♪', '_', '<i>', '</i>', '\r', '[', ']', '(', ')'
+]
+
 fn.lerDiretorio(caminho)
-    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+    .then(fn.elementosTerminadosCom('.srt'))
+    .then(fn.lerArquivos)
+    .then(conteudos => conteudos.join('\n'))
+    .then(todoConteudo => todoConteudo.split('\n'))
+    .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosSeIncluir('-->'))
+    .then(fn.removerElementosSeApenasNumero)
+    .then(fn.removerSimbolos(simbolos))
     .then(console.log)
